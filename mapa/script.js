37add5b97375a75a962d7817035fb3d874cd39fb
PascualHache <hector.pascual.diaz@gmail.com>
2020-03-09 13:04:45 +0100

    Selection works!

--- mapa/script.js
+++ mapa/script.js
@@ -1,8 +1,12 @@
-var map = L.map('mapid').on('load', onMapLoad).setView([41.400, 2.206], 9);
-//map.locate({setView: true, maxZoom: 17});
+var map = L.map('mapid').on('load', onMapLoad).setView([41.400, 2.206], 16);
+map.locate({setView: true, maxZoom: 17});
 
 var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
+var layerGroup = L.layerGroup().addTo(map);
+
+
+var popup = L.popup();
 //en el clusters almaceno todos los markers
 var markers = L.markerClusterGroup();
 var data_markers = [];
@@ -14,26 +18,88 @@ $(document).ready(function () {
 		dataType: "json",
 		url: "api/apiRestaurants.php",
 		success: function (data) {
-			alert('Get Success');
+			// alert('Get Success');
 			console.log(data);
 			db_parsed = data;
 		}
 	});
 
-	
+
 });
 
-function test() {
-	$(db_parsed).each(function(index) {
+function getKindRest() {
+	$(db_parsed).each(function (index) {
 		var res = db_parsed[index]["kind_food"].split(',');
 		for (let n = 0; n < res.length; n++) {
-			if(kind_food_selector.indexOf(res[n]) !== -1){
-			} else{
+			if (kind_food_selector.indexOf(res[n]) !== -1) {
+			} else {
 				kind_food_selector.push(res[n]);
 			}
 		};
-	});
-	console.log(kind_food_selector)
+	}); 
+	return kind_food_selector
+	// console.log(kind_food_selector)
+}
+
+function getRestByKind(kind) {
+	let result=[];
+	if (kind == "all") {
+		$(db_parsed).each(function (index) {
+			result.push(db_parsed[index]);
+		});
+	} else {
+		$(db_parsed).each(function (index) {
+			var res = db_parsed[index]["kind_food"].split(',');
+			if (res.includes(kind)) {
+				
+				result.push(db_parsed[index]);
+			}
+		});
+	} drawByObj(result);
+	// return result;
+}
+
+function drawByObj(inputDb){
+	// layerGroup.clearLayers();
+	loadSelectOptions();
+	$(inputDb).each(function (index) {
+		L.marker([inputDb[index].lat, inputDb[index].lng], { draggable: true }).addTo(layerGroup);
+		// L.marker([41.400, 2.206], { draggable: true }).addTo(layerGroup);
+        popup
+            .setLatLng({"lat":parseInt(inputDb[index].lat), "lng":inputDb[index].lng})
+            // .setContent("<strong>" + inputDb[index].name+"<br>"+inputDb[index].kind_food)
+			.openOn(map);
+	}); 
+	map.setView(new L.LatLng(41.400, 2.206));
+}
+
+function fitFocusScreen(inputDb){
+	let position = [];
+	for (let index = 0; index < inputDb.length; index++) {
+		position.push(L.latLng(inputDb[index].lat, inputDb[index].lng));
+	}
+
+	bounds = L.latLngBounds(position);
+	map.fitBounds(bounds);
+}
+
+function loadSelectOptions(){
+	let arr = getKindRest();
+	select = document.getElementById('kind_food_selector');
+	while (select.firstChild) {
+		select.removeChild(select.lastChild);
+	  }
+
+	var opt = document.createElement('option');
+	opt.innerHTML = "Todos";
+	select.appendChild(opt);
+	
+	for (let index = 0; index < arr.length; index++) {
+		opt = document.createElement('option');
+		opt.innerHTML = arr[index];
+		select.appendChild(opt);
+	}
+	
 }
 
 function onMapLoad() {
