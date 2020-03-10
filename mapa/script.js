ea1092682d52096969eb19ec91308c8046846b40
PascualHache <hector.pascual.diaz@gmail.com>
2020-03-10 11:10:02 +0100

    focus better

--- mapa/script.js
+++ mapa/script.js
@@ -12,21 +12,39 @@ var markers = L.markerClusterGroup();
 var data_markers = [];
 var kind_food_selector = [];
 var db_parsed = {};
-$(document).ready(function () {
-	$.ajax({
-		type: "GET",
-		dataType: "json",
-		url: "api/apiRestaurants.php",
-		success: function (data) {
-			// alert('Get Success');
-			console.log(data);
-			db_parsed = data;
-		}
-	});
 
-
+//generación de la base de datos
+// $(document).ready(function () {
+// 	$.ajax({
+// 		type: "GET",
+// 		dataType: "json",
+// 		async: false,
+//         cache: false,
+// 		url: "api/apiRestaurants.php",
+// 		success: function (data) {
+// 			// alert('Get Success');
+// 			console.log(data);
+// 			db_parsed = data;
+// 		}
+// 	});
+// });
+function getDB(){
+return $.ajax({
+	type: "GET",
+	dataType: "json",
+	async: false,
+	cache: false,
+	url: "api/apiRestaurants.php",
+	success: function (data) {
+		// alert('Get Success');
+		console.log(data);
+		db_parsed = data;
+	}
 });
+}
 
+
+//obtención de array de tipos de restaurantes
 function getKindRest() {
 	$(db_parsed).each(function (index) {
 		var res = db_parsed[index]["kind_food"].split(',');
@@ -41,6 +59,7 @@ function getKindRest() {
 	// console.log(kind_food_selector)
 }
 
+//Obtención de objeto con los restautrantes filtrado por tipo (entrada) 
 function getRestByKind(kind) {
 	let result=[];
 	if (kind == "all") {
@@ -55,24 +74,27 @@ function getRestByKind(kind) {
 				result.push(db_parsed[index]);
 			}
 		});
-	} drawByObj(result);
-	// return result;
+	} //drawByObj(result);
+	return result;
 }
 
+//Función que pinta markers en función de objeto pre filtrado(entrada)
 function drawByObj(inputDb){
 	// layerGroup.clearLayers();
-	loadSelectOptions();
+	// loadSelectOptions();
 	$(inputDb).each(function (index) {
 		L.marker([inputDb[index].lat, inputDb[index].lng], { draggable: true }).addTo(layerGroup);
 		// L.marker([41.400, 2.206], { draggable: true }).addTo(layerGroup);
         popup
             .setLatLng({"lat":parseInt(inputDb[index].lat), "lng":inputDb[index].lng})
-            // .setContent("<strong>" + inputDb[index].name+"<br>"+inputDb[index].kind_food)
+            .setContent("<strong>" + inputDb[index].name+"<br>"+inputDb[index].kind_food)
 			.openOn(map);
 	}); 
-	map.setView(new L.LatLng(41.400, 2.206));
+	
+	// map.setView(new L.LatLng(41.400, 2.206));
 }
 
+//Permite enfocar todos los marcadores en pantalla
 function fitFocusScreen(inputDb){
 	let position = [];
 	for (let index = 0; index < inputDb.length; index++) {
@@ -83,6 +105,7 @@ function fitFocusScreen(inputDb){
 	map.fitBounds(bounds);
 }
 
+//Pinta las opciones en el select
 function loadSelectOptions(){
 	let arr = getKindRest();
 	select = document.getElementById('kind_food_selector');
@@ -96,15 +119,27 @@ function loadSelectOptions(){
 	
 	for (let index = 0; index < arr.length; index++) {
 		opt = document.createElement('option');
+		opt.value=arr[index];
 		opt.innerHTML = arr[index];
 		select.appendChild(opt);
 	}
 	
 }
 
+$.when(getDB()).done(function(data){
+	console.log("Pausa!");
+	loadSelectOptions();
+	drawByObj(getRestByKind('all'));
+	fitFocusScreen(db_parsed);
+	
+
+});
+
 function onMapLoad() {
 
 	console.log("Mapa cargado");
+	
+
     /*
 	FASE 3.1
 		1) Relleno el data_markers con una petición a la api
@@ -115,14 +150,25 @@ function onMapLoad() {
 }
 
 $('#kind_food_selector').on('change', function () {
+	// var optionSelected = $("option:selected", this);
+    // var valueSelected = this.value;
 	console.log(this.value);
+	layerGroup.clearLayers();
+	drawByObj(getRestByKind(this.value));
+	// document.getElementById('#kind_food_selector').value = valueSelected;
 	render_to_map(data_markers, this.value);
 });
 
 
 
 function render_to_map(data_markers, filter) {
-
+	if (filter == "all"){
+		console.log("all");
+		getRestByKind("all");
+	} else{
+		console.log(filter);
+		getRestByKind(filter);
+	}
 	/*
 	FASE 3.2
 		1) Limpio todos los marcadores
